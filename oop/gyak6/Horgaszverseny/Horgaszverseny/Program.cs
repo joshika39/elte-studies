a52ba633a56d4fd58a386e5ac6f6d8d7c13f334a
Joshua Hegedus <jhegedus9@gmail.com>
2023-04-06 11:19:23 +0200

    bead

---
+++ oop/gyak6/Horgaszverseny/Horgaszverseny/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Implementation.Core;
+using Infrastructure.IO;
+using Unity;
+
+namespace Horgaszverseny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = Guid.NewGuid();
+            var c = Bootsrapper.GetDefaultContainer("log.txt", id);
+            var reader = c.Resolve<IReader>();
+            var file = new StreamReader(@"Resources/input.txt");
+            
+            var test = reader.ReadLine<Fisher>(file, Fisher.TryParse);
+            
+            foreach (var fisher in test)
+            {
+                Console.Write(fisher.Name);
+                foreach (var @catch in fisher.Cathces)
+                {
+                    Console.Write($"{@catch.Name} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
