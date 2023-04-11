5e0983d4b63296dc3b0dbc51fb4c0f23841c57ab
Joshua Hegedus <jhegedus9@gmail.com>
2023-04-11 12:32:43 +0200

    java

--- prog_nyelvek/gyak1/Main.java
+++ prog_nyelvek/gyak1/Main.java
@@ -4,7 +4,10 @@ import gyak1.tasks.Odd;
 
 public class Main {
 	public static void main(String[] args){
-		var odd = new Odd();
-		odd.run(args);
+		int[] nums = {0,1,2,3,4,5,6};
+java.util.Arrays.stream(nums).filter( i -> i%2==0 )
+                             .map( i -> i/2 )
+                             .limit(3)
+                             .forEach( System.out::print );
 	}
 }
