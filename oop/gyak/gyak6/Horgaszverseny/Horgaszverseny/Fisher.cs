2e29814f2b4a514df041d63e8b9c8f0f69c8f439
Joshua Hegedus <47304430+joshika39@users.noreply.github.com>
2023-04-17 23:26:03 +0900

    OOP (#6)
    * bead
    
    * fixed some of the issues
    
    * updated packages
    
    * changed version
    
    * asd
    
    * ZH1
    
    * created hf2
    
    * update to 0.2.1.1
    
    * some fixes
    
    * fixed hf 2
    
    * added zip
    
    * fixed so many stuff
    
    ---------
    
    Co-authored-by: Joshua Hegedus <jhegedus9@gmail.com>

---
+++ oop/gyak/gyak6/Horgaszverseny/Horgaszverseny/Fisher.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Horgaszverseny
+{
+    public class Fisher
+    {
+        public string Name { get; set; }
+        public List<Catch> Cathces { get; set; }
+
+        public Fisher(string name)
+        {
+            Name = name;
+            Cathces = new List<Catch>();
+        }
+        
+        public Fisher(string name, List<Catch> catches)
+        {
+            Name = name;
+            Cathces = catches;
+        }
+
+        public override string ToString()
+        {
+            var catchesStr = "";
+            foreach (var @catch in Cathces)
+            {
+                catchesStr += $"\t{@catch.Time}: {@catch.Name}, hossz: {@catch.Length}, suly: {@catch.Weight}\n";
+            }
+            return $"Horgasz: {Name}\n{catchesStr}";
+        }
+
+        public static bool TryParse(string line, out Fisher result)
+        {
+            var elements = line.Split(' ').ToList();
+            elements.RemoveAll(string.IsNullOrWhiteSpace);
+            var name = elements[0];
+            elements.RemoveAt(0);
+            var i = 0;
+            var catches = new List<Catch>();
+            var tmpCatch = new Catch();
+            foreach (var catchData in elements)
+            {
+                var pos = i % 4;
+                switch (pos)
+                {
+                    case 0:
+                        tmpCatch = new Catch
+                        {
+                            Time = catchData
+                        };
+                        break;
+                    case 1:
+                        tmpCatch.Name = catchData;
+                        break;
+                    case 2:
+                        tmpCatch.Weight = double.Parse(catchData);
+                        break;
+                    case 3:
+                        tmpCatch.Length = double.Parse(catchData);
+                        catches.Add(tmpCatch);
+                        break;
+                }
+                i++;
+            }
+            result = new Fisher(name, catches);
+            return true;
+        }
+    }
+}
