7b69bafa10bdddefa3520d1090254956b7078827
Joshua Hegedus <jhegedus9@gmail.com>
2023-05-05 20:53:07 +0200

    haskell bead 9 + web

--- semester_II/oop/gyak/NagyBead/Main/Program.cs
+++ semester_II/oop/gyak/NagyBead/Main/Program.cs
@@ -7,22 +7,28 @@ using LibraryCore.Lib;
 using LibraryCore.People;
 Console.WriteLine("Hello, World!");
 
-ILibrary lib = new LibraryClass(new BookFactory());
-IMember member = new Member("asd", ":asd", new DateTime(), lib);
+int[,] tomb = new int[4,4];
+tomb[0, 0] = 3;
+tomb[1, 0] = 3;
+tomb[2, 0] = 3;
+tomb[3, 0] = 3;
 
-IAuthor ks = new Author("Kovács Sándor", new DateTime(1652, 3, 24));
-var diffEqDraft = ks.WriteBook("Differenciaegyenletek", 456);
-
-IPublisher typoTex = new Publisher();
-var diffEq = typoTex.Publish("978-963-4930-99-0", diffEqDraft, new DateTime(2020, 02, 12));
-ks.Books.Add(diffEq);
-
-lib.GetNewBook(diffEq, Constants.BookTypes.SCIENCE_BOOK);
-var joshua = new Member("Joshua Hegedus", "joshika39", new DateTime(2003, 01, 17), lib);
-lib.Register(joshua);
-
-lib.LendBook(joshua ,diffEq, new DateTime(2023, 04, 05));
-lib.ReturnBook(joshua, diffEq, new DateTime(2023, 05, 25));
+// ILibrary lib = new LibraryClass(new BookFactory());
+// IMember member = new Member("asd", ":asd", new DateTime(), lib);
+//
+// IAuthor ks = new Author("Kovács Sándor", new DateTime(1652, 3, 24));
+// var diffEqDraft = ks.WriteBook("Differenciaegyenletek", 456);
+//
+// IPublisher typoTex = new Publisher();
+// var diffEq = typoTex.Publish("978-963-4930-99-0", diffEqDraft, new DateTime(2020, 02, 12));
+// ks.Books.Add(diffEq);
+//
+// lib.GetNewBook(diffEq, Constants.BookTypes.SCIENCE_BOOK);
+// var joshua = new Member("Joshua Hegedus", "joshika39", new DateTime(2003, 01, 17), lib);
+// lib.Register(joshua);
+//
+// lib.LendBook(joshua ,diffEq, new DateTime(2023, 04, 05));
+// lib.ReturnBook(joshua, diffEq, new DateTime(2023, 05, 25));
 
 
 
--- semester_II/web/kreastol/js/main.js
+++ semester_II/web/kreastol/js/main.js
@@ -2,15 +2,18 @@ $(document).ready(function () {
   $.get(
     "layout/nav.html",
     function (html_string) {
-      var doc = new DOMParser().parseFromString(html_string, "text/html");
-      let nav = doc.getElementsByTagName("nav").item(0);
-      //   console.log(nav);
+      var navdoc = new DOMParser().parseFromString(
+        html_string,
+        "application/xhtml+xml"
+      );
 
+      let nav = navdoc.getElementsByTagName("nav").item(0);
       let navTitle = nav.getElementsByTagName("h1").item(0);
+      if (document.title == "Kezdőlap") {
+        addSidewayMove();
+      }
       navTitle.classList.add("title");
-      //   console.log(navTitle);
       navTitle.textContent = document.title;
-      console.log(nav.innerHTML);
 
       document.getElementById("nav").innerHTML = nav.innerHTML;
     },
@@ -18,28 +21,31 @@ $(document).ready(function () {
   );
 });
 
-const slider = document.querySelector('.events');
-let mouseDown = false;
-let startX, scrollLeft;
+function addSidewayMove() {
+  const slider = document.querySelector(".events");
+  let mouseDown = false;
+  let startX, scrollLeft;
 
-let startDragging = function (e) {
-  mouseDown = true;
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-};
-let stopDragging = function (event) {
-  mouseDown = false;
-};
+  let startDragging = function (e) {
+    mouseDown = true;
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  };
+  let stopDragging = function (event) {
+    mouseDown = false;
+  };
 
-slider.addEventListener('mousemove', (e) => {
-  e.preventDefault();
-  if(!mouseDown) { return; }
-  const x = e.pageX - slider.offsetLeft;
-  const scroll = x - startX;
-  slider.scrollLeft = scrollLeft - scroll;
-});
+  slider.addEventListener("mousemove", (e) => {
+    e.preventDefault();
+    if (!mouseDown) {
+      return;
+    }
+    const x = e.pageX - slider.offsetLeft;
+    const scroll = x - startX;
+    slider.scrollLeft = scrollLeft - scroll;
+  });
 
-// Add the event listeners
-slider.addEventListener('mousedown', startDragging, false);
-slider.addEventListener('mouseup', stopDragging, false);
-slider.addEventListener('mouseleave', stopDragging, false);
+  slider.addEventListener("mousedown", startDragging, false);
+  slider.addEventListener("mouseup", stopDragging, false);
+  slider.addEventListener("mouseleave", stopDragging, false);
+}
