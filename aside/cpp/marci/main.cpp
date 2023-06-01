9d8ee4439c58b6700d981ab5d0d2836e592411f0
Joshua Hegedus <jhegedus9@gmail.com>
2023-06-01 14:23:24 +0200

    changed to linked lists

--- aside/cpp/marci/main.cpp
+++ aside/cpp/marci/main.cpp
@@ -8,17 +8,67 @@ typedef struct Move {
 } Move;
 
 typedef struct Bucket {
-    int Top;
-    int Bottom;
+    int base;
+    Bucket *top;
 } Bucket;
 
-bool getNeighbour(int from, int size, Bucket *buckets, int &neighbour) {
-    if (buckets[from].Bottom == 0) {
+int getHeight(Bucket* bucket){
+    if(bucket == nullptr){
+        return 0;
+    }
+
+    int i = 1;
+    while (bucket->top != nullptr){
+        bucket = bucket->top;
+        i++;
+    }
+    return i;
+}
+
+int getHighestLevel(Bucket** buckets, int size){
+    int maxHeight = getHeight(*(buckets));
+    for (int i = 0; i < size; ++i) {
+        int newHeight = getHeight(*(buckets + i));
+        if(newHeight > maxHeight){
+            maxHeight = newHeight;
+        }
+    }
+
+    return maxHeight;
+}
+
+Bucket* getTop(Bucket* bucket){
+    while (bucket->top != nullptr){
+        bucket = bucket->top;
+    }
+    return bucket;
+}
+
+Bucket* getNth(Bucket* bucket, int n){
+    auto h = getHeight(bucket);
+    if(h < n || bucket == nullptr){
+        return nullptr;
+    }
+    int i = 0;
+    bool found = false;
+    while (bucket->top != nullptr && i < n){
+        found = true;
+        bucket = bucket->top;
+        i++;
+    }
+    if(found){
+        return bucket;
+    }
+    return nullptr;
+}
+
+bool getNeighbour(int from, int size, Bucket **buckets, int &neighbour) {
+    if (buckets[from] == nullptr) {
         return false;
     }
 
     for (int i = from + 1; i < size; i++) {
-        if (buckets[i].Bottom != 0) {
+        if (buckets[i] != nullptr) {
             neighbour = i;
             return true;
         }
@@ -27,11 +77,8 @@ bool getNeighbour(int from, int size, Bucket *buckets, int &neighbour) {
 }
 
 bool getDifference(Bucket b1, Bucket b2, int &diff){
-    if((b1.Top == b2.Bottom) || (b1.Bottom == b2.Top)){
-        return false;
-    }
-    int test1 = abs(b1.Top - b2.Bottom);
-    int test2 = abs(b1.Bottom - b2.Top);
+    int test1 = abs(getTop(&b1)->base- b2.base);
+    int test2 = abs(b1.base - getTop(&b2)->base);
 
     if(test1 < test2){
         diff = test1;
@@ -42,7 +89,7 @@ bool getDifference(Bucket b1, Bucket b2, int &diff){
     return true;
 }
 
-bool smallestGap(Bucket *buckets, int size, int &n1, int &n2) {
+bool smallestGap(Bucket **buckets, int size, int &n1, int &n2) {
     if (size < 2) {
         return false;
     }
@@ -50,10 +97,12 @@ bool smallestGap(Bucket *buckets, int size, int &n1, int &n2) {
     bool success = true;
     int min = 1000;
     for (int i = 0; i < size - 1 && success; i++) {
-        if (buckets[i].Bottom != 0) {
+        Bucket* curr = buckets[i];
+        if (curr != nullptr) {
             success = getNeighbour(i, size, buckets, neighbour);
             int newVal;
-            success = getDifference(buckets[i], buckets[neighbour], newVal);
+            auto nB = *(buckets + neighbour);
+            success = getDifference(*curr, *nB, newVal);
             if (i != neighbour && newVal < min) {
                 min = newVal;
                 n1 = i, n2 = neighbour;
@@ -63,47 +112,56 @@ bool smallestGap(Bucket *buckets, int size, int &n1, int &n2) {
     return success;
 }
 
-void printBuckets(Bucket *buckets, int size) {
+void printBucketsOnLevel(Bucket **buckets, int size, int level){
     for (int i = 0; i < size; ++i) {
-        if(buckets[i].Bottom == buckets[i].Top){
-            printf("   ");
+        if(level == 0){
+            if(buckets[i] == nullptr){
+                printf("00 ");
+            }
+            else{
+                if(buckets[i]->base < 10){
+                    printf("%d  ", buckets[i]->base);
+                }else{
+                    printf("%d ", buckets[i]->base);
+                }
+            }
         }
         else{
-            if(buckets[i].Top < 10){
-                printf("%d  ", buckets[i].Top);
-            }else{
-                printf("%d ", buckets[i].Top);
+            auto bucket = getNth(buckets[i], level);
+            if(bucket == nullptr){
+                printf("   ");
+            }
+            else{
+                if(bucket->base < 10){
+                    printf("%d  ", bucket->base);
+                }else{
+                    printf("%d ", bucket->base);
+                }
             }
         }
     }
-
     cout << "\n";
-    for (int i = 0; i < size; ++i) {
-        if(buckets[i].Bottom == 0){
-            printf("00 ");
-        }
-        else {
-            if(buckets[i].Bottom < 10){
-                printf("%d  ", buckets[i].Bottom);
-            }else{
-                printf("%d ", buckets[i].Bottom);
-            }
-        }
+}
+
+void printBuckets(Bucket **buckets, int size) {
+    int highestLevel = getHighestLevel(buckets, size);
+    for (int i = highestLevel - 1; i >= 0; i--) {
+        printBucketsOnLevel(buckets, size, i);
     }
-    cout << "\n\n";
+    cout << "\n";
 }
 
 int main() {
     bool success = true;
-    int bucketCount = 10;
-    int input[] = {9, 7, 5, 3, 1, 2, 4, 6, 8, 10};
+    int bucketCount = 20;
+    int input[] = {10, 13, 14, 12, 11, 19, 20, 18, 17, 16, 15, 9, 8, 6, 7, 5, 4, 3, 2, 1};
 
-    Bucket buckets[bucketCount];
+    auto buckets = new Bucket *[bucketCount];
     for (int i = 0; i < bucketCount; ++i) {
-        Bucket bucket;
-        bucket.Bottom = input[i];
-        bucket.Top = input[i];
-        buckets[i] = bucket;
+        auto bucket = new Bucket;
+        bucket->base = input[i];
+        bucket->top = nullptr;
+        *(buckets + i) = bucket;
     }
 
     Move moves[bucketCount];
@@ -113,22 +171,23 @@ int main() {
         success = smallestGap(buckets, bucketCount, n1, n2);
 
         Move tmpMove;
-        if (buckets[n1].Top < buckets[n2].Bottom) {
+        auto b1 = buckets[n1];
+        auto b2 = buckets[n2];
+        if (getTop(b1)->base < b2->base) {
             tmpMove.From = n1 + 1;
             tmpMove.To = n2 + 1;
-            buckets[n2].Top = buckets[n1].Top;
-            buckets[n1].Bottom = 0;
-            buckets[n1].Top = 0;
-        } else if (buckets[n2].Top < buckets[n1].Bottom) {
+            buckets[n2]->top = b1;
+            buckets[n1] = nullptr;
+        } else if (getTop(b2)->base < b1->base) {
             tmpMove.From = n2 + 1;
             tmpMove.To = n1 + 1;
-            buckets[n1].Top = buckets[n2].Top;
-            buckets[n2].Bottom = 0;
-            buckets[n2].Top = 0;
+            buckets[n1]->top = b2;
+            buckets[n2] = nullptr;
         }
         moves[i] = tmpMove;
         printBuckets(buckets, bucketCount);
     }
+
     if(success){
         for (int i = 0; i < bucketCount - 1; ++i) {
             Move curr = moves[i];
