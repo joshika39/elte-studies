99038e3cd35f4b7d79befc7fb34b720d1cc0ac4d
Joshua Hegedus <josh.hegedus@outlook.com>
2023-09-18 22:09:01 +0200

    from arch

--- semester_III/Algo_II/main.cpp
+++ semester_III/Algo_II/main.cpp
@@ -1,89 +1,180 @@
 #include <iostream>
 #include <algorithm>
 
-struct Node {
+class Node {
+public:
     int key;
     Node* left;
     Node* right;
-    int height;
+    int balance;
+
+    Node() {
+        left = nullptr;
+        right = nullptr;
+        balance = 0;
+    }
+
+    Node(int key) {
+        this->key = key;
+        left = nullptr;
+        right = nullptr;
+        balance = 0;
+    }
 };
 
-Node* AVLremMax(Node* root);
 
-int getHeight(Node* node) {
-    if (node == nullptr)
-        return 0;
-    return node->height;
+void balanceMMm(Node* &tree, Node* &left) {
+    tree -> left = left -> right;
+    left -> right = tree;
+    left -> balance = 0;
+    tree -> balance = 0;
+    tree = left;
 }
 
-int getBalanceFactor(Node* node) {
-    if (node == nullptr)
-        return 0;
-    return getHeight(node->left) - getHeight(node->right);
+void balanceMMp(Node* &tree, Node* &left) {
+    Node* right = left -> right;
+    tree -> left = right -> right;
+    left -> right = right -> left;
+    right -> right = tree;
+    right -> left = left;
+    tree -> balance = (right -> balance + 1) / 2;
+    right -> balance = (1 - right -> balance) / 2;
+    right -> balance = 0;
+    tree = right;
 }
 
-Node* rotateRight(Node* y) {
-    Node* x = y->left;
-    Node* T2 = x->right;
-    x->right = y;
-    y->left = T2;
-    y->height = std::max(getHeight(y->left), getHeight(y->right)) + 1;
-    x->height = std::max(getHeight(x->left), getHeight(x->right)) + 1;
-
-    return x;
+void balancePPm(Node* &tree, Node* &right) {
+    Node* left = right -> left;
+    tree -> right = left -> left;
+    right -> left = left -> right;
+    left -> left = tree;
+    left -> right = right;
+    tree -> balance = (left -> balance + 1) / 2;
+    right -> balance = (1 - left -> balance) / 2;
+    left -> balance = 0;
+    tree = left;
 }
 
-Node* rotateLeft(Node* x) {
-    Node* y = x->right;
-    Node* T2 = y->left;
-    y->left = x;
-    x->right = T2;
-    x->height = std::max(getHeight(x->left), getHeight(x->right)) + 1;
-    y->height = std::max(getHeight(y->left), getHeight(y->right)) + 1;
-
-    return y;
+void balancePPp(Node* &tree, Node* &right) {
+    tree -> right = right -> left;
+    right -> left = tree;
+    right -> balance = 0;
+    tree -> balance = 0;
+    tree = right;
 }
 
-Node* balanceMM0(Node* node) {
-    int balance = getBalanceFactor(node);
-
-    if (balance > 1 && getBalanceFactor(node->left) >= 0) {
-        return rotateRight(node);
+void leftSubtreeGrown(Node* &t, bool &d) {
+    if(t -> balance == -1) {
+        Node* left = t -> left;
+        if(left -> balance == -1) {
+            balanceMMm(t, left);
+        }
+        else{
+            balanceMMp(t, left);
+        }
+        d = false;
     }
-    if (balance > 1 && getBalanceFactor(node->left) < 0) {
-        node->left = rotateLeft(node->left);
-        return rotateRight(node);
+    else{
+        t -> balance--;
+        d = t -> balance < 0;
     }
-    if (balance < -1 && getBalanceFactor(node->right) <= 0) {
-        return rotateLeft(node);
-    }
-    if (balance < -1 && getBalanceFactor(node->right) > 0) {
-        node->right = rotateRight(node->right);
-        return rotateLeft(node);
-    }
-
-    return node;
 }
 
-Node* AVLremMax(Node* root) {
-    if (root == nullptr) {
-        return nullptr;
+void rightSubtreeGrown(Node* &t, bool &d) {
+    if(t -> balance == 1) {
+        Node* right = t -> right;
+        if(right -> balance == 1) {
+            balancePPp(t, right);
+        }
+        else {
+            balancePPm(t, right);
+        }
+        d = false;
     }
+    else{
+        t -> balance++;
+        d = t -> balance > 0;
+    }
+}
 
-    if (root->right == nullptr) {
-        Node* maxNode = root;
-        root = root->left;
-        delete maxNode;
-        return root;
+void AVLInsert(Node* &t, int k, bool &d) {
+    if(t == nullptr) {
+        if(k < t -> key) {
+            AVLInsert(t -> left, k, d);
+        }
+        if(d) {
+            leftSubtreeGrown(t, d);
+        }
+
+        if(k > t -> key) {
+            AVLInsert(t -> right, k, d);
+        }
+         if(d) {
+             rightSubtreeGrown(t, d);
+         }
     }
-    root->right = AVLremMax(root->right);
-    root = balanceMM0(root);
-    return root;
+    else{
+        d = false;
+    }
+}
+
+void printTree(Node* root, std::string prefix = "", bool isLeft = true) {
+    if (root != nullptr) {
+        std::cout << (isLeft ? "L--" : "R--");
+
+        std::cout << root->key << " (Balance: " << root->balance << ")" << std::endl;
+
+        // Recursively print the left and right subtrees
+        printTree(root->left, prefix + (isLeft ? "|   " : "    "), true);
+        printTree(root->right, prefix + (isLeft ? "|   " : "    "), false);
+    }
+}
+
+void printBinaryTree(Node* root, std::string prefix = "", bool isLeft = true) {
+    if (root != nullptr) {
+        std::cout << (isLeft ? "L--" : "R--");
+        std::cout << root->key << std::endl;
+
+        // Recursively print the left and right subtrees
+        printBinaryTree(root->left, prefix + (isLeft ? "|   " : "    "), true);
+        printBinaryTree(root->right, prefix + (isLeft ? "|   " : "    "), false);
+    }
+}
+
+void printPlainTree(Node* root, int indent = 0) {
+    if (root != nullptr) {
+        // Print right subtree with increased indentation
+        printPlainTree(root->right, indent + 4);
+
+        // Print the current node with appropriate indentation
+        for (int i = 0; i < indent; i++) {
+            std::cout << " ";
+        }
+        std::cout << root->key << "\n";
+
+        // Print left subtree with increased indentation
+        printPlainTree(root->left, indent + 4);
+    }
+}
+
+void deleteTree(Node* root) {
+    if (root != nullptr) {
+        deleteTree(root->left);
+        deleteTree(root->right);
+        delete root;
+    }
 }
 
 int main() {
-    Node* root = nullptr;
-    root = AVLremMax(root);
+    Node* root = new Node(10);
+    root->left = new Node(5);
+    root->right = new Node(15);
+    root->left->left = new Node(2);
+    root->left->right = new Node(7);
+
+    printPlainTree(root);
+    AVLInsert(root, 1);
+    deleteTree(root);
 
     return 0;
 }
