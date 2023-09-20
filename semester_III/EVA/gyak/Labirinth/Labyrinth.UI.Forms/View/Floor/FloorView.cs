77f0c75e53cdfc04d99c5b0ed8cdfa5aa10c4168
Joshua Hegedus <josh.hegedus@outlook.com>
2023-09-20 08:51:50 +0200

    progress

--- semester_III/EVA/gyak/Labirinth/Labyrinth.UI.Forms/View/Floor/FloorView.cs
+++ semester_III/EVA/gyak/Labirinth/Labyrinth.UI.Forms/View/Floor/FloorView.cs
@@ -1,12 +1,30 @@
-﻿using Labyrinth.UI.Forms.View.Floor._Interfaces;
+﻿using Labyrinth.BL.Labyrinth;
+using Labyrinth.BL.Player;
+using Labyrinth.UI.Forms.View.Floor._Interfaces;
 
 namespace Labyrinth.UI.Forms.View.Floor
 {
-    public partial class FloorView : UserControl, IFloorView
+    public partial class FloorView : UserControl, IFloorView, ILabyrinth2D
     {
         public FloorView()
         {
             InitializeComponent();
+        }
+        public int SizeX
+        {
+            get;
+        }
+        public int SizeY
+        {
+            get;
+        }
+        public IEnumerable<IPlayer2D> Players
+        {
+            get;
+        }
+        public ILabyrinthLayer LabyrinthLayer
+        {
+            get;
         }
     }
 }
