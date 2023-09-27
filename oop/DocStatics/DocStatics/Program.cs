09500ac919745d1b5f9ff357d02703792426fe51
Joshua Hegedus <josh.hegedus@outlook.com>
2023-09-27 09:22:45 +0200

    moved project and removed ui framework

---
+++ oop/DocStatics/DocStatics/Program.cs
@@ -0,0 +1,20 @@
+﻿using Implementation.IO.Factories;
+using Implementation.Logger.Factories;
+
+namespace DocStatics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ioFactory = new IOFactory();
+            var id = Guid.NewGuid();
+            var logger = new LoggerFactory().CreateLogger(id);
+            var writer = ioFactory.CreateWriter(logger);
+            var reader = ioFactory.CreateReader(logger, writer);
+            var text = reader.ReadAllLines("Kerek egy hosszu szoveget: ");
+
+            Console.WriteLine(text);
+        }
+    }
+}
