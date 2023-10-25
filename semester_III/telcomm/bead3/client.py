bb37655a342d9041d873718cc799a55155395376
Joshua Hegedus <josh.hegedus@outlook.com>
2023-10-25 20:48:01 +0200

    finished bead

--- semester_III/telcomm/bead3/client.py
+++ semester_III/telcomm/bead3/client.py
@@ -1,22 +1,49 @@
-from socket import socket, AF_INET, SOCK_DGRAM, timeout, SOL_SOCKET, SO_REUSEADDR
+import random
+import socket
 import struct
 import sys
+import time
 
-TCP_IP = "localhost"
-TCP_PORT = int(sys.argv[1])
-BUFFER_SIZE = 1024
-packer = struct.Struct('ci')
-server_addr = (TCP_IP, TCP_PORT)
 
-with socket(AF_INET, SOCK_DGRAM) as client:
-    client.connect(server_addr)
+def main():
+    if len(sys.argv) != 3:
+        print("Használat: python3 client.py <hostname> <port szám>")
+        return
 
-    data, address = client.recvfrom(4096)
-    received = packer.unpack(data)
-    print(received)
-    while received != 'K' or received != 'N' or received != 'Y':
-        packed_data = packer.pack()
-        sent = client.sendto(packed_data, server_addr)
+    host = sys.argv[1]
+    port = int(sys.argv[2])
 
-        data, address = client.recvfrom(4096)
-        received = packer.unpack(data)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
+    guess = 50
+    multiplier = 25
+    while True:
+        time_to_wait = random.randint(1, 3)
+        print(f"Sleeping for {time_to_wait}, then guessing: {guess} -> ({round(guess)})")
+        time.sleep(time_to_wait)
+        if multiplier <= 1:
+            client_socket.send(struct.pack('cI', b'=', round(guess)))
+        else:
+            client_socket.send(struct.pack('cI', b'<', round(guess)))
+        response = client_socket.recv(1024)
+        result, _ = struct.unpack('cI', response)
+        if result == b'Y':
+            print("Nyertél!")
+            break
+        elif result == b'N':
+            guess = guess + multiplier
+            multiplier = multiplier / 2
+        elif result == b'I':
+            guess = guess - multiplier
+            multiplier = multiplier / 2
+        elif result == b'V':
+            print("A játék már véget ért.")
+            break
+        elif result == b'K':
+            print("Kiestél")
+            break
+    client_socket.close()
+
+
+if __name__ == "__main__":
+    main()
--- semester_III/telcomm/bead3/server.py
+++ semester_III/telcomm/bead3/server.py
@@ -1,45 +1,90 @@
 import socket
+import random
 import struct
 import sys
+
 import select
 
-TCP_IP = "localhost"  # "127.0.0.1" or ""
-TCP_PORT = int(sys.argv[1])
-BUFFER_SIZE = 1024
-reply = "Hello kliens".encode()  # could be just b"Hello kliens"
+host = sys.argv[1]
+port = int(sys.argv[2])
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((TCP_IP, TCP_PORT))
+clients = []
+target_number = random.randint(1, 100)
 
-inputs = [sock]
-timeout = 1.0
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((host, port))
+server_socket.listen(5)
 
-unpacker = struct.Struct('ci')
+print(f"Véletlenszerű szám: {target_number}")
+game_finished = False
 
-while True:
+while not game_finished:
     try:
-        readables, _, _ = select.select(inputs, [], [], timeout)
-
-        for s in readables:
-            if s is sock:
-                connection, client_info = sock.accept()
-                print(f"Someone has connected: {client_info[0]}:{client_info[1]}")
-                inputs.append(connection)
+        readable, _, _ = select.select([server_socket] + clients, [], [])
+        for sock in readable:
+            if sock is server_socket:
+                client, _ = server_socket.accept()
+                clients.append(client)
             else:
-                data, address = sock.recvfrom(4096)
-                if not data:
-                    s.close()
-                    print("The client has terminated the connection")
-                    inputs.remove(s)
-                    continue
-                unp_data = unpacker.unpack(data)
-                print("Unpack:", unp_data)
-                x = eval(str(unp_data[0]) + unp_data[2].decode() + str(unp_data[1]))
-                sent = sock.sendto(data, address)
-                print("Sent response:", sent)
-
+                data = sock.recv(1024)
+                if data:
+                    message, guess = struct.unpack('cI', data)
+                    if game_finished:
+                        sock.send(struct.pack('cI', *(b'K', 0)))
+                        continue
+                    if message == b'>':
+                        if guess > target_number:
+                            response = (b'N', 0)
+                        elif guess == target_number:
+                            response = (b'Y', 0)
+                            game_finished = True
+                        else:
+                            response = (b'I', 0)
+                    elif message == b'<':
+                        if guess < target_number:
+                            response = (b'N', 0)
+                        elif guess == target_number:
+                            response = (b'Y', 0)
+                            game_finished = True
+                        else:
+                            response = (b'I', 0)
+                    elif message == b'=':
+                        if guess == target_number:
+                            response = (b'Y', 0)
+                            game_finished = True
+                        else:
+                            response = (b'N', 0)
+                    else:
+                        response = (b'V', 0)
+                    sock.send(struct.pack('cI', *response))
+                else:
+                    sock.close()
+                    clients.remove(sock)
     except KeyboardInterrupt:
-        for s in inputs:
+        for s in clients:
             s.close()
         print("Server closing")
         break
+
+while True:
+    try:
+        readable, _, _ = select.select([server_socket] + clients, [], [])
+        for sock in readable:
+            if sock is server_socket:
+                client, _ = server_socket.accept()
+                clients.append(client)
+            else:
+                data = sock.recv(1024)
+                if data:
+                    message, guess = struct.unpack('cI', data)
+                    response = (b'V', 0)
+                    sock.send(struct.pack('cI', *response))
+                else:
+                    sock.close()
+                    clients.remove(sock)
+    except KeyboardInterrupt:
+        for s in clients:
+            s.close()
+        print("Server closing")
+        break
+
