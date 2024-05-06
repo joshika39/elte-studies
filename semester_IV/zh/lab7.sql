f04ef6e6971177552f7dc9577af93de7ba832ca5
Joshua Hegedus <josh.hegedus@outlook.com>
2024-05-06 07:03:39 +0200

    finished lab7

--- semester_IV/zh/lab7.sql
+++ semester_IV/zh/lab7.sql
@@ -81,3 +81,69 @@ BEGIN
     RETURN a;
 END;
 /
+
+-- 4
+/* Írjunk meg egy függvényt, amelyik megadja, hogy hányszor fordul elő egy 
+   karakterláncban -> p1 egy másik részkarakterlánc -> p2 
+*/
+-- CREATE OR REPLACE FUNCTION hanyszor(p1 VARCHAR2, p2 VARCHAR2) RETURN integer IS
+-- Tesztelés:
+SELECT hanyszor ('ab c ab ab de ab fg', 'ab') FROM dual;
+
+CREATE OR REPLACE FUNCTION hanyszor(p1 VARCHAR2, p2 VARCHAR2) RETURN INTEGER IS
+    cnt INTEGER := 0;
+    pos INTEGER := 1;
+BEGIN
+    LOOP
+        pos := INSTR(p1, p2, pos);
+        IF pos = 0 THEN
+            EXIT;
+        END IF;
+        cnt := cnt + 1;
+        pos := pos + LENGTH(p2);
+    END LOOP;
+    
+    RETURN cnt;
+END;
+/
+
+-- 5
+/* Írjunk meg egy függvényt, amelyik visszaadja a paraméterként szereplő '+'-szal 
+   elválasztott számok összegét.
+*/
+-- CREATE OR REPLACE FUNCTION osszeg(p_char VARCHAR2) RETURN number IS
+-- Tesztelés:
+SELECT osszeg('1 + 4 + 13 + -1 + 0') FROM dual;
+
+CREATE OR REPLACE FUNCTION osszeg(p_char VARCHAR2) RETURN NUMBER IS
+    total NUMBER := 0;
+    pos INTEGER := 1;
+    len INTEGER;
+    num VARCHAR2(100);
+BEGIN
+    len := LENGTH(p_char);
+    
+    WHILE pos <= len LOOP
+        num := TRIM(SUBSTR(p_char, pos, INSTR(p_char, '+', pos) - pos));
+        BEGIN
+            total := total + TO_NUMBER(num);
+        EXCEPTION
+            WHEN VALUE_ERROR THEN
+                NULL;
+        END;
+        pos := INSTR(p_char, '+', pos);
+        IF pos = 0 THEN
+            EXIT;
+        END IF;
+        pos := pos + 1;
+    END LOOP;
+    BEGIN
+        num := TRIM(SUBSTR(p_char, pos));
+        total := total + TO_NUMBER(num);
+    EXCEPTION
+        WHEN VALUE_ERROR THEN
+            NULL;
+    END;
+    RETURN total;
+END;
+/
