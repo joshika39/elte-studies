10915f8ebbe1530f227948339b0b1696fb37e90a
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-09-27 09:52:46 +0200

    serveroldali progress

---
+++ semester_V/szerveroldali/ticket/app/Models/Ticket.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'done',
+        'priority',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'done' => 'boolean',
+        ];
+    }
+}
