e84262a81ddd80e2e09032ae94ecb251938e56ed
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-10-04 20:21:52 +0200

    added thread safe mutable from last class

--- semester_V/Konkurens/src/main/gy3/ThreadSafeMutableIntArray.java
+++ semester_V/Konkurens/src/main/gy3/ThreadSafeMutableIntArray.java
@@ -1,23 +1,32 @@
 package main.gy3;
 
+import java.util.Arrays;
+
 public class ThreadSafeMutableIntArray {
     private int capacity;
-    private final Object lock = new Object();
+    private Object[] locks;
     private final int[] array;
 
     public ThreadSafeMutableIntArray(int capacity) {
         this.capacity = capacity;
         this.array = new int[capacity];
+        this.locks = new Object[capacity];
+    }
+
+    public ThreadSafeMutableIntArray(int[] array) {
+        this.capacity = array.length;
+        this.array = Arrays.copyOf(array, capacity);
+        this.locks = new Object[capacity];
     }
 
     public int get(int index) {
-        synchronized (lock) {
+        synchronized (locks[index]) {
             return array[index];
         }
     }
 
     public void set(int index, int value) {
-        synchronized (lock) {
+        synchronized (locks[index]) {
             array[index] = value;
         }
     }
