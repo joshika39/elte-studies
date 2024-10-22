a1086b8cc5ce0ff31ed20d0319dd8afc1a3488aa
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-10-22 14:28:08 +0200

    added changes

--- semester_V/ab2/gyak3.sql
+++ semester_V/ab2/gyak3.sql
@@ -1,27 +1,42 @@
-CREATE OR REPLACE PROCEDURE num_of_rows IS
-    CURSOR c_blocks IS
-        SELECT file_id, block_id, block_id + blocks - 1 AS end_block
-        FROM dba_extents
-        WHERE segment_name = 'TABLA_123'
-          AND owner = 'NIKOVITS'
-        ORDER BY file_id, block_id;
-    
-    v_file_id    NUMBER;
-    v_block_id   NUMBER;
-    v_end_block  NUMBER;
-    v_row_count  NUMBER;
+SET SERVEROUTPUT ON;
+
+CREATE OR REPLACE PROCEDURE NUM_OF_ROWS IS
+    CURSOR C_BLOCKS IS
+    SELECT
+        FILE_ID,
+        BLOCK_ID,
+        BLOCK_ID + BLOCKS - 1 AS END_BLOCK
+    FROM
+        DBA_EXTENTS
+    WHERE
+        SEGMENT_NAME = 'TABLA_123'
+        AND OWNER = 'NIKOVITS'
+    ORDER BY
+        FILE_ID,
+        BLOCK_ID;
+    V_FILE_ID   NUMBER;
+    V_BLOCK_ID  NUMBER;
+    V_END_BLOCK NUMBER;
+    V_ROW_COUNT NUMBER;
 BEGIN
-    FOR block_rec IN c_blocks LOOP
-        v_block_id := block_rec.block_id;
-        v_end_block := block_rec.end_block;
-        
-        WHILE v_block_id <= v_end_block LOOP
-            SELECT COUNT(*) INTO v_row_count
-            FROM nikovits.tabla_123
-            WHERE DBMS_ROWID.ROWID_BLOCK_NUMBER(rowid) = v_block_id
-              AND DBMS_ROWID.ROWID_RELATIVE_FNO(rowid) = block_rec.file_id;
-            DBMS_OUTPUT.PUT_LINE(block_rec.file_id || ';' || v_block_id || '->' || v_row_count || ';');
-            v_block_id := v_block_id + 1;
+    FOR BLOCK_REC IN C_BLOCKS LOOP
+        V_BLOCK_ID := BLOCK_REC.BLOCK_ID;
+        V_END_BLOCK := BLOCK_REC.END_BLOCK;
+        WHILE V_BLOCK_ID <= V_END_BLOCK LOOP
+            SELECT
+                COUNT(*) INTO V_ROW_COUNT
+            FROM
+                NIKOVITS.TABLA_123
+            WHERE
+                DBMS_ROWID.ROWID_BLOCK_NUMBER(ROWID) = V_BLOCK_ID
+                AND DBMS_ROWID.ROWID_RELATIVE_FNO(ROWID) = BLOCK_REC.FILE_ID;
+            DBMS_OUTPUT.PUT_LINE(BLOCK_REC.FILE_ID
+                                 || ';'
+                                 || V_BLOCK_ID
+                                 || '->'
+                                 || V_ROW_COUNT
+                                 || ';');
+            V_BLOCK_ID := V_BLOCK_ID + 1;
         END LOOP;
     END LOOP;
 END;
